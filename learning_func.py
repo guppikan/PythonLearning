ddf57eafd7b9040069557fa5dc34161c0805e25f
guppikan <guppikan83@yahoo.com>
2019-09-23 21:50:15 +0530

    This is second file about basic syntax on function

--- learning_func.py
+++ learning_func.py
@@ -1,9 +1,9 @@
 #Author : Guru prasad Raju 
-#Fucntion signature with parameter name 
+#Function signature with parameter name 
 def welcom_greetings(name):
 
 #Printing message 
 	print("Namaste: "+name)
 
-#Calling fucntions with argument 'Ram'
+#Calling function with argument 'Ram'
 welcom_greetings('Ram')
