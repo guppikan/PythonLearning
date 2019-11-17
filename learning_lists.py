0ead09fdf9a3df27ee0cf48e3b42497007fbbeb2
guppikan <guppikan83@yahoo.com>
2019-11-17 13:14:44 +0530

    This new file for python list

---
+++ learning_lists.py
@@ -0,0 +1,12 @@
+# Author  Guru Prasad Raju 
+
+# Python lists 
+
+# initialization and declaration of new list(Note: list are enclosed in close brackets)
+new_list=['this','is','new','list']
+
+print(new_list) 
+
+
+# accessing individual elements in the list 
+print(new_list[2])
