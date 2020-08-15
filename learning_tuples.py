e2c9841c86e50848e06c8723e792865b75bcb689
guppikan <guppikan83@yahoo.com>
2020-08-15 12:46:12 +0530

    Tuples python file

---
+++ learning_tuples.py
@@ -0,0 +1,12 @@
+# Author  Guru Prasad Raju 
+
+# Python lists 
+
+# initialization and declaration of new tuple(Note: tuples are enclosed in  brackets)
+new_tuple=('this','is','new','tuple')
+
+print(new_tuple) 
+
+
+# accessing individual elements in the list 
+print(new_tuple[3])
