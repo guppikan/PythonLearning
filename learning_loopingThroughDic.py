56dfb193058a54a795faa435b1f75a087b371e1b
guppi <guppikan83@yahoo.com>
2020-08-30 06:40:43 +0000

    This is file for looping through dictionary

---
+++ learning_loopingThroughDic.py
@@ -0,0 +1,19 @@
+# this porgramme describes how to loop through dictonary
+
+thisDict3={
+    "Name":"test2",
+    "Designation" :"Manager",
+    "Age":34
+}
+
+# looping  thourgh dic and printing 
+
+for x in thisDict3:
+    print(x)
+
+print("*************")
+ #Print all values in the dictionary, one by one:
+
+for x in thisDict3:
+     print(thisDict3[x])
+      
