e923ca7866a8f15a9b560f237d7852c03c4c7c88
guppi <guppikan83@yahoo.com>
2020-08-30 06:28:37 +0000

    This file describe on how to access an item on dictionary

---
+++ learning_accItemInDic.py
@@ -0,0 +1,12 @@
+# this porgramme describes how to access an item from dictonary
+
+thisDict2={
+    "Name":"test1",
+    "Designation" :"Devloper",
+    "Age":24
+}
+
+# Accessing an item from dic and printing
+
+x =thisDict2['Designation']
+print(x)
