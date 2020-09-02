1fdde6da53a92858640b5087565f1004a84e22a9
guppi <guppikan83@yahoo.com>
2020-09-02 08:37:24 +0000

    This is file for changing values on Dictionaries

---
+++ learning_ChangingValuesInDict.py
@@ -0,0 +1,12 @@
+# this program shows on how to change values in dictionaries
+
+thisDic3 = {"Name": "test1",
+            "Born": 1989,
+            "Gender": "Male"}
+print("Original dictionary: ", thisDic3)
+
+# changing dictionary value "Born" from 1989 to 2020
+
+thisDic3["Born"] = 2020
+
+print("The updated dictionary :", thisDic3)
