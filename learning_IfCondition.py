87c7c3993d91bdc050f34fb96670389f4d78d000
guppi <guppikan83@yahoo.com>
2020-09-04 09:39:05 +0000

    This is python if condition file

---
+++ learning_IfCondition.py
@@ -0,0 +1,11 @@
+# if loop learning
+
+# author: Guru Prasad Raju
+x = 100  # declaring variable x and assiging value 100 to it
+y = 40  # declaring variable y and assiging value  40 to it
+
+if x < y:     # condtional statement
+    print("x is less than y")
+
+else:
+    print("Y greater than x")
