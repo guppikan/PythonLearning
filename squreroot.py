d7556cd0c177000b1ac691206a32d5b0d659a281
Guru Prasad <guppikan83@yahoo.com>
2020-12-26 11:41:15 +0530

    How to calculate square

---
+++ squreroot.py
@@ -0,0 +1,12 @@
+# this file shows how to create squre root function
+import math
+
+# initalizing variable a with value
+a = 25
+# initalizing variable b with value
+
+b = 38
+
+# calling squre root function
+squreRoot = math.sqrt(a*a+b*b)
+print("The squre root value :", squreRoot)
