5795d3eb17609c4ef3cf3baa087cd1c2609eae35
Guru Prasad <guppikan83@yahoo.com>
2021-01-17 19:27:18 +0530

    This is file which describe init function

---
+++ learning_init.py
@@ -0,0 +1,12 @@
+# This programming will show how init method work
+
+class Car:
+    # this is act as constuctor is for class car and its not "Constructor"
+    def __init__(self):
+        print("the method ___init___ is called")
+
+
+# instansiating class car
+ford = Car()
+maruthi = Car()
+audi = Car()
