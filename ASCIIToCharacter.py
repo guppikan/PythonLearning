9770470bfa7a6d557369868d5dcdef484f7e7385
guppi <guppikan83@yahoo.com>
2021-05-24 17:41:27 +0530

    This program converts a string character to ASCII code

---
+++ ASCIIToCharacter.py
@@ -0,0 +1,11 @@
+# Author : Guru prasad Raju
+
+''' This program converts a ASCII value to a character'''
+
+'''Python 'ord()' : It coverts the given string of length one,
+ return an integer representing the unicode code point
+  of the character'''
+
+a = 'g'
+
+print("The ASCII value of '" + a + "'is", ord(a))
