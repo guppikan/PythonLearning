5cc3603db9c9e06ff458e24ca0b8990793f72053
guppi <guppikan83@yahoo.com>
2021-06-01 07:36:23 +0530

    This program will create inner and outer function to avoid loop

---
+++ innerOuter.py
@@ -0,0 +1,21 @@
+# Author : Guru prasad Raju
+'''This program will create inner and outer function 
+ within functionto avoid loop'''
+
+# creating a function
+
+
+def outerFunction(a, b):
+    claculating_square = a**2
+    # creating inner function
+
+    def innerFunction(a, b):
+        return a+b
+
+    addPlusFive = innerFunction(a, b)
+    return addPlusFive+5
+
+
+# calling function
+finalResult = outerFunction(7, 8)
+print(f'Printing value of outer function:{finalResult}')
